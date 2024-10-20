58b93a015b45a16d6e6e40877d85b2174255de90
Lovice <123145620+lovistics@users.noreply.github.com>
2024-10-20 09:25:36 +0300

    Update settings.py- security key removed

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*f7u0j$=&7)b5-q8w+1ig$lusy^@ihmo18%9!wuvn*-&c+isq%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
